ab896e429a08a3c7a02ec3a9d0be2035a6139707
Vitali Karmanov <vitalik2@byui.edu>
2020-05-24 23:39:05 -0600

    Added Sitemap generator

---
+++ Controllers/SitemapController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleMvcSitemap;
+
+namespace ASPNET_Core_MVC_SEO_Friendly_Template.Controllers
+{
+    public class SitemapController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<SitemapNode> nodes = new List<SitemapNode>
+        {
+            new SitemapNode(Url.Action("Index","Home")),
+            new SitemapNode(Url.Action("Privacy","Home")),
+            new SitemapNode(Url.Action("Index","Products")),
+            new SitemapNode(Url.Action("ProductExample","Products")),
+            //other nodes
+        };
+
+            return new SitemapProvider().CreateSitemap(new SitemapModel(nodes));
+        }
+    }
+}

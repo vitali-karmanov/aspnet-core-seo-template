24677740a519e2423b0e32271c2a32fe8d4fdcf0
Vitali Karmanov <vitalik2@byui.edu>
2020-05-24 18:30:07 -0600

    Added ASP.NET Core MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNET_Core_MVC_SEO_Friendly_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
